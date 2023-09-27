bc5a5c9577991d384432d54e01e551a6feccf314
jborelli95 <jborelli95@gmail.com>
2023-09-27 09:44:06 -0300

    Probando Json

--- Borelli/Trabajos/test/json_test/js/app.js
+++ Borelli/Trabajos/test/json_test/js/app.js
@@ -1,1 +1,15 @@
-console.log("Hello world!");
+const url = "https://pokeapi.co/api/v2/pokemon?offset=20&limit=1272"
+
+const getApi = async(url) => {
+    try{
+        const resultado = await fetch(url);
+
+        const data = await resultado.json();
+
+        console.log(data);
+    }catch{
+        console.log("Error 404!: Error grave");
+    }
+}
+
+getApi(url);
